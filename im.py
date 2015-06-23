2de52a41c7672e7e97aef5ee0d2d4da43a1f9554
Devin Higgins <higgi135.mail.lib.msu.edu>
2015-06-23 18:03:55 -0400

    Update command for creating JP2

--- im.py
+++ im.py
@@ -9,7 +9,6 @@ class ImageMagickConverter(Derivatives):
         self.config_section = image_type
         self.get_configs()
 
-
     def convert_jpeg_low(self, input_tif, output_jpg):
 
         self.name = "JPEG low-quality conversion"
@@ -34,10 +33,10 @@ class ImageMagickConverter(Derivatives):
         self.return_code = self.run_cmds()
         self.print_output()
 
-    def convert_islandora_jpg(self, input_tif, output_jpg):
+    def convert_jp2(self, input_tif, output_jpg):
 
-        self.name = "Islandora JPG conversion"
-        self.cmds = self.islandora_jpg.replace("image.tif", input_tif).replace("image.jpg", output_jpg).split()
+        self.name = "JP2 conversion"
+        self.cmds = self.jp2.replace("image.tif", input_tif).replace("image.jp2", output_jpg).split()
         self.print_process()
         self.return_code = self.run_cmds()
         self.print_output()
